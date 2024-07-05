b5824781621cbb9822c8feff830aef18d467e4bc
juned khan <junedkhanfarh@gmail.com>
2024-07-05 11:47:10 +0530

    Added video History function and also routed all the user controller function written till now

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudnary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens= async(userId)=>{
     try {
@@ -384,6 +385,55 @@ const getUserChannelProfile= asynchadnler(async(req,res)=>{
 
 })
 
+//now to get history of user(video history)
+
+const getVideoHistory= asynchadnler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"Video",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+
+    ])
+    return res.status(200).json(new ApiResponse(200,user[0].watchHistory,"watch history fetched succefully"))
+})
+
+
 
 
 
@@ -397,5 +447,6 @@ export {registerUser,
     updateAccountDetails,
     Userupdateavatar,
     UserupdateCoverimage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getVideoHistory
 }
--- src/routes/user.routers.js
+++ src/routes/user.routers.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getVideoHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, Userupdateavatar, UserupdateCoverimage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -28,6 +28,16 @@ router.route("/login").post(loginUser)
 router.route("/logout").post( verifyJwt,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 
+router.route("/change-pasword").post(verifyJwt,changeCurrentPassword)
+router.route("/Current-user").get(verifyJwt,getCurrentUser)
 
+router.route("/update-details").patch(verifyJwt,updateAccountDetails)
+router.route("/update-avatar").patch(verifyJwt,upload.single("avatar"),Userupdateavatar)
+router.route("/coverImage-update").patch(verifyJwt,upload.single("CoverImage"),UserupdateCoverimage)
+
+
+//now for req.params
+router.route("/c/:username").get(verifyJwt,getUserChannelProfile)
+router.route("/history").get(verifyJwt,getVideoHistory)
 
 export default router
