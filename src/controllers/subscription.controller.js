ef541d1ffe672bfe10654f78214e66a6543b212b
juned khan <junedkhanfarh@gmail.com>
2024-07-21 19:05:23 +0530

    written first controller of subscriptio

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -8,6 +8,34 @@ import { asynchadnler } from "../utils/asynhandler.js"
 const toggleSubscription = asynchadnler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+
+    try {
+        const AlreadySubscriber= await Subscription.find(
+            {
+                channel:channelId
+            }
+        )
+        if(AlreadySubscriber.length===0)
+            {
+                const subscribed = await Subscription.create({
+                    subscriber:req.user?._id,
+                    channel:channelId
+                    })
+                if(!subscribed){throw new ApiError(500,"couldn't create entry in db")}
+    
+                res.status(200).json(new ApiResponse(200,subscribed,"Subscribed the channel"))
+        }
+        else{
+            const unsubscriber= await Subscription.deleteOne({_id:AlreadySubscriber[0]._id})
+    
+            res.status(200).json(new ApiResponse(200,unsubscriber,"Unsubscribed the Channel"))
+        
+                }
+    } catch (error) {
+        res.status(503).json(503,error,"Couldn't Subscriber or unsubscribe")
+        
+    }
+
 })
 
 // controller to return subscriber list of a channel
